eb432b781f52acb5acbdbc17040e7bc867fa218b
[디플루이드] 이현우 <hyunwoo1@lgupluspartners.co.kr>
2025-12-30 11:18:14 +0900

    프론트 화면 작업중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+'use client';
+import { useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './page.scss';
+import 'swiper/css';
+
 export default function Home() {
+  useEffect(() => {
+    document.body.style.background =
+      'linear-gradient(122.01deg, #FAF5FF 0%, #FCE7F3 50%, #FFEDD5 100%), #FFFFFF';
+  }, []);
+
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
-        Frontend
-      </h1>
+    <div className="pretendard h-screen">
+      <header className="h-[86px] bg-[#ccc] px-[23px] pt-[21px]">
+        <h1 className="text-[24px] leading-[29px] font-bold">서비스명</h1>
+      </header>
+      <div className="relative h-[calc(100vh-86px)]">
+        <Swiper direction="vertical" className="justify-center">
+          <SwiperSlide className="h-auto max-w-[402px] bg-white">
+            asdfsad
+          </SwiperSlide>
+          <SwiperSlide className="h-auto max-w-[402px] bg-white">
+            asdfsaasdsadd
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
